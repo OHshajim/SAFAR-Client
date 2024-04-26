17410b65206e3046b010b4813a0b1b118933f256
OHshajim <ajshajimmax@gmail.com>
2024-04-26 16:12:52 +0600

    spots data show

--- src/assets/Components/SpotsCard.jsx
+++ src/assets/Components/SpotsCard.jsx
@@ -1,9 +1,20 @@
 
 const SpotsCard = ({ spot }) => {
-    const { name, email, spotName, location, country, seasonality, travel_time, visitorsPerYear, average_cost, image, description } = spot;
+    const {  spotName, location, country, seasonality, travel_time, visitorsPerYear, average_cost, image, description } = spot;
     return (
         <div>
-
+            <div className="card  bg-base-100 shadow-xl max-h-[600px]">
+                <figure><img src={image} alt={spotName} className="m-4 rounded-lg"/></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{spotName}</h2>
+                    {
+                        (description.length>100)? <p>{description.slice(0,100)}...</p>: <p>{description}</p>
+                    }
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">View Details</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
