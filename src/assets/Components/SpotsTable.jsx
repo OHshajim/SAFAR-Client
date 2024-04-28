d68a1a67b9b6006d80ef36db9078e08e47f7ee5c
OHshajim <ajshajimmax@gmail.com>
2024-04-28 20:40:03 +0600

    my list  spots data delete

--- src/assets/Components/SpotsTable.jsx
+++ src/assets/Components/SpotsTable.jsx
@@ -1,12 +1,10 @@
 import { FaDeleteLeft } from "react-icons/fa6";
 
-const SpotsTable = ({ spot}) => {
-    const { tourists_spot_name, country_Name, travel_time, average_cost } = spot;
+const SpotsTable = ({ spot, handleDelete }) => {
 
-    const handleDelete =()=>{
-        
-    }
-    const handleUpdate =()=>{
+    const { _id, tourists_spot_name, country_Name, travel_time, average_cost } = spot;
+
+    const handleUpdate = () => {
 
     }
     return (
@@ -16,8 +14,8 @@ const SpotsTable = ({ spot}) => {
                 <td>{country_Name}</td>
                 <td>{travel_time}</td>
                 <td>{average_cost}$</td>
-                <td><button onClick={handleUpdate} className="btn">Update</button></td>
-                <td><button onClick={handleDelete} className="btn text-2xl bg-transparent border-red-500 text-red-500"><FaDeleteLeft /></button></td>
+                <td><button onClick={()=>handleUpdate(_id)} className="btn bg-transparent border-cyan-500 text-cyan-500">Update</button></td>
+                <td><button onClick={()=>handleDelete(_id)} className="btn text-2xl bg-transparent border-red-500 text-red-500"><FaDeleteLeft /></button></td>
             </tr>
         </tbody>
     );
--- src/assets/Pages/MySpots.jsx
+++ src/assets/Pages/MySpots.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import SpotsCard from "../Components/SpotsCard";
 import { AuthContext } from "../../Provider/AuthProvider";
 import SpotsTable from "../Components/SpotsTable";
+import Swal from "sweetalert2";
 
 const MySpots = () => {
     const [spots, setSpots] = useState([])
@@ -11,28 +12,60 @@ const MySpots = () => {
         fetch(`http://localhost:5000/spots/${user?.email}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                // console.log(data);
                 setSpots(data)
             })
 
     }, [user])
+    const handleDelete = (_id) => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/spots/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your spot has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = spots.filter(spot => spot._id !== _id)
+                            setSpots(remaining)
+                        }
+                    })
+            }
+        })
+    };
     return (
         <div className="max-w-[1600px] py-20 mx-auto">
             <h1 className="text-5xl my-10">My List </h1>
             <div className="overflow-x-auto ">
-                <table className="table border rounded-xl md:p-10 border-separate bg-[#4848482c]">
+                <table className="table border rounded-xl md:p-10 border-separate bg-[#1e36402c]">
                     <thead className="border-b border-white">
-                        <tr className="text-white">
-                            <th className="">Spot Name</th>
-                            <th className="">Country</th>
-                            <th className="">Travel Time</th>
-                            <th className="">Average Cost</th>
-                            <th className="">Update</th>
-                            <th className="">Delete</th>
+                        <tr className="text-cyan-100">
+                            <th >Spot Name</th>
+                            <th >Country</th>
+                            <th >Travel Time</th>
+                            <th >Average Cost</th>
+                            <th >Update</th>
+                            <th >Delete</th>
                         </tr>
                     </thead>
                     {
-                        spots.map((spot) => <SpotsTable key={spot._id} spot={spot} />)
+                        spots.map((spot) => <SpotsTable key={spot._id} spot={spot} handleDelete={handleDelete} />)
                     }
 
                 </table>
